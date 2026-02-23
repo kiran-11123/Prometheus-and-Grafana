ac78fa9cb782192ec51e4a1b6dbf8e4e031edf45
Kiran <kiran.sadara6730@gmail.com>
2026-02-23 19:35:51 +0530

    Increment count

--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,9 @@ import express from 'express';
 //prom-express
 const app = express();
 
-
 app.use(express.json());
 
-function middleware(req :any,res :any,next:any){ 
-     
-    const startTime = Date.now();
-    next();
 
-    const endTime   = Date.now() - startTime ;
-
-    console.log("Total Time took is " , endTime , "ms"  );
-
- 
-}
-
-app.use(middleware);
 
 
 
@@ -40,9 +27,9 @@ app.get("/user" , async (req,res)=>{
 
 
 
-app.post("/user" ,middleware ,  async (req , res)=>{
+app.post("/user"  ,  async (req , res)=>{
      
-    return res.status(200).json({
+     res.status(200).json({
         message : "First POST request"
     })
 })
---
+++ src/monitoring/requestCount.ts
@@ -0,0 +1,25 @@
+import  client from 'prom-client'
+
+const requestCounter = new client.Counter({
+    name:  "Request_Count",
+    help:"Total request count",
+    labelNames : ["method" , "route"]
+     
+})
+
+export function requestCount(req :any,res:any,next:any) { 
+
+    requestCounter.inc({
+
+      
+
+        method : req.method,
+        route : req.route.path,
+         
+    });
+
+    next();
+
+        
+
+} 
